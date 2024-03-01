a45ff1af11d86a6cec35106f2715283f2a2bc6d1
Morgane Soula <morganes56@gmail.com>
2024-03-01 14:02:40 +0100

    fix: AGP from 8.2.2 to 8.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("androidx.compose.runtime:runtime:1.6.1")
+        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("androidx.compose.runtime:runtime:1.6.2")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
         classpath("com.google.gms:google-services:${PluginVersion.G_SERVICES}")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.2.2")
-    implementation("androidx.compose.runtime:runtime:1.6.1")
+    implementation("com.android.tools.build:gradle:8.3.0")
+    implementation("androidx.compose.runtime:runtime:1.6.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     implementation("com.squareup:javapoet:1.13.0")
 }
