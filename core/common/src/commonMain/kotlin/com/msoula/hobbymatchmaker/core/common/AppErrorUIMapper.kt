a043197d7fd179edcbdc0c1a2d9875f59b17100f
morganesoula <morganes56@gmail.com>
2025-08-22 12:02:01 +0200

    add: ui text mapping for exception

--- core/common/src/commonMain/kotlin/com/msoula/hobbymatchmaker/core/common/AppErrorUIMapper.kt
+++ core/common/src/commonMain/kotlin/com/msoula/hobbymatchmaker/core/common/AppErrorUIMapper.kt
@@ -1,5 +1,6 @@
 package com.msoula.hobbymatchmaker.core.common
 
+import com.msoula.hobbymatchmaker.core.common.UIText.*
 import org.jetbrains.compose.resources.StringResource
 
 sealed interface UIText {
@@ -13,30 +14,33 @@ interface ErrorMessageMapper {
 
 object DefaultErrorMessageMapper : ErrorMessageMapper {
     override fun toUIText(error: AppError): UIText = when (error) {
-        is AppError.Network.Timeout -> UIText.Resource(Res.string.network_timeout)
-        is AppError.Network.Unreachable -> UIText.Resource(Res.string.network_unreachable)
-        is AppError.Network.Http -> UIText.Resource(
+        AppError.Network.Timeout -> Resource(Res.string.network_timeout)
+        AppError.Network.Unreachable -> Resource(Res.string.network_unreachable)
+        is AppError.Network.Http -> Resource(
             Res.string.http_error_code, listOf(error.code)
         )
 
-        is AppError.Network.Canceled -> UIText.Resource(Res.string.request_canceled)
-        is AppError.Network.Serialization -> UIText.Resource(Res.string.serialization_error)
-        is AppError.Network.Unknown -> UIText.Resource(Res.string.unknown_error)
+        AppError.Network.Canceled -> Resource(Res.string.request_canceled)
+        AppError.Network.Serialization -> Resource(Res.string.serialization_error)
+        is AppError.Network.Unknown -> Resource(Res.string.unknown_error)
 
-        is AppError.Domain.Validation -> UIText.Resource(
+        is AppError.Domain.Validation -> Resource(
             Res.string.validation_error, listOf(error.reason)
         )
 
-        is AppError.Domain.Unauthorized -> UIText.Resource(Res.string.unauthorized)
-        is AppError.Domain.Forbidden -> UIText.Resource(Res.string.forbidden)
-        is AppError.Domain.NotFound -> UIText.Resource(Res.string.not_found)
+        AppError.Domain.Unauthorized -> Resource(Res.string.unauthorized)
+        AppError.Domain.Forbidden -> Resource(Res.string.forbidden)
+        AppError.Domain.NotFound -> Resource(Res.string.not_found)
 
-        is AppError.External.Service -> UIText.Resource(
+        is AppError.External.Service -> Resource(
             Res.string.external_service_error, listOf(error.provider)
         )
 
-        is AppError.Storage.WriteFailed -> UIText.Plain("")
-        is AppError.Storage.ReadFailed -> UIText.Plain("")
-        is AppError.Storage.Corrupted -> UIText.Plain("")
+        AppError.Storage.WriteFailed -> Resource(Res.string.storage_write_failed)
+        AppError.Storage.ReadFailed -> Resource(Res.string.storage_read_failed)
+        AppError.Storage.Corrupted -> Resource(Res.string.storage_corrupted)
+
+        AppError.Authentication.AlreadyExists -> Plain("")
+        AppError.Authentication.Unknown -> Plain("")
     }
 }
